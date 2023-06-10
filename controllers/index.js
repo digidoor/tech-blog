6bfe4fcdd553609adb347364d7e70aa80d3720c9
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-09 19:57:54 -0700

    gutted everything, got a simple test route working

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
 
-router.use('/api', apiRoutes);
+router.get('/', (req, res) =>
+{
+    res.json({ name: 'bob', role: 'be bob' });
+});
+
 
 module.exports = router;
