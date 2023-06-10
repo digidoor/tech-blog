0b5260ebd99a2b84ccfbb47d173d12f69a883eff
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-09 21:04:10 -0700

    finished models, moving to seeds

--- models/index.js
+++ models/index.js
@@ -2,6 +2,5 @@ const User = require('./User');
 const Blog = require('./Blog');
 
 User.hasMany(Blog, { foreignKey: 'user_id', onDelete: 'CASCADE', });
-
-
+//above line was after the export in our project; 
 module.exports = { User, Blog };
