d7d27120aaac2b0d993a9e184b58421f1d6a0acf
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-09 20:24:30 -0700

    deleting index.html worked, all handlebars now

--- controllers/index.js
+++ controllers/index.js
@@ -6,10 +6,4 @@ router.get('/', (req, res) =>
     res.render('home');
 });
 
-router.get('/handle', (req, res) =>
-{
-    //res.sendFile(path.join(__dirname, 'public/index.html'));
-    res.render('home');
-});
-
 module.exports = router;
