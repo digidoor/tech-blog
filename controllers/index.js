be3543e969d281f9a4d696e73a6fdcb145826738
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-09 20:23:03 -0700

    / keeps bypassing handlebars, but why?

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,14 @@ const router = require('express').Router();
 
 router.get('/', (req, res) =>
 {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    //res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.render('home');
 });
 
+router.get('/handle', (req, res) =>
+{
+    //res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.render('home');
+});
 
 module.exports = router;
